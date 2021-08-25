6739c7adbce28c3bbaa3da9034b4bfc25d68d014
RishiIsCool <87836869+RishiIsCool@users.noreply.github.com>
2021-08-24 21:41:39 -0400

    Add files via upload

---
+++ js/ComputerBase.js
@@ -0,0 +1,23 @@
+class ComputerBase{
+    constructor(x,y,w,h){
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options)
+        World.add(world,this.body)
+        this.w = w
+        this.h = h
+        this.image = loadImage(".assets/base2.png")
+    }   
+    display(){
+    var pos = this.body.position
+    var angle = this.body.angle
+
+    push()
+    translate(pos.x,pos.y)
+    rotate(angle)
+    imageMode(CENTER)
+    image(this.image,0,0,this.width,this.height)
+    pop();
+    }
+}
